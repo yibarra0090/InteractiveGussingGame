2ef0731aafaadea83df10a847db1b22180581b04
BT_1N3_14@BKTECH.LOCAL <BT_1N3_14@BKTECH.LOCAL>
2018-10-10 14:41:36 -0400

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,9 @@
+//Yussef Ibarra
+import java.util.Scanner;
+public class Game {
+    public static void InteractiveGuessingGame(int num){
+        Scanner input = new Scanner (System.in);
+        
+    }
+
+}
