3490ff2959ce5c0e78083289b3505c32bd49beb0
BT_1N3_14 <yibarra0090@bths.edu>
2018-10-11 14:48:00 -0400

    Done

--- src/Game.java
+++ src/Game.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Game {
     public static void main (String[] args){
         Scanner input = new Scanner (System.in);
-        int guess = 0;
         int num = (int)((Math.random()*11)+1);
         System.out.println("Guess the number between 1 and 10:");
-        guess = input.nextInt();
+        int guess = input.nextInt();
+        int numOfguess = 1;
         while (guess != num){
             System.out.println("Guess Again:");
             guess = input.nextInt();
+            numOfguess += 1;
         }
         System.out.println("Good Job!");
+        System.out.println("It took you"+" "+""+numOfguess+""+" "+""+"guess(es) to get the right answer");
     }
-
-
 }
