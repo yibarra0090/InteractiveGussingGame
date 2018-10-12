a21e4134f0ece2f2377a26d4cf920f4471038924
BT_1N3_14 <yibarra0090@bths.edu>
2018-10-12 14:48:50 -0400

    Hello

---
+++ src/AIGame.java
@@ -0,0 +1,12 @@
+//Yussef Ibarra
+import java.util.Scanner;
+
+public class AIGame {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        int guess = (int)(Math.random()*100)+1;
+        System.out.println("Is your number"+""+guess+""+"?(type correct, higher, or lower):");
+        while
+
+    }
+}
